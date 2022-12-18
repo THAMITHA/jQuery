da999640b1df94780ab7e37bc1e98dc3c2e5b61d
Thamitha <thamitharajput@gmail.com>
2022-12-18 11:56:31 +0530

    Code using jQuery

---
+++ script.js
@@ -0,0 +1,12 @@
+//  without jQuery using plain javascript
+
+//var para1 = document.getElementById('para1');
+//para1.innerHTML="Welcome";
+
+// with using jQuery
+
+$('#para1').html('Welcome');
+// $('#para1').hide();
+
+
+// $('p').html('welcome');

25eb7e512327cd5c0321adebeae350195eac10d1
Thamitha <thamitharajput@gmail.com>
2022-12-18 12:26:55 +0530

    Modifying CSS Using Query

--- script.js
+++ script.js
@@ -5,8 +5,23 @@
 
 // with using jQuery
 
-$('#para1').html('Welcome');
+// $('#para1').html('Welcome');
 // $('#para1').hide();
 
 
 // $('p').html('welcome');
+
+// $('p').css('color','red');
+// $('p').css('fontSize','30px');
+
+// setting multiple css properties
+$('p').css({
+    fontSize: "100px",
+    color: "red"
+});
+
+$('div').css({
+    width: "100px",
+    height: "100px",
+    backgroundColor: "cyan"
+})
